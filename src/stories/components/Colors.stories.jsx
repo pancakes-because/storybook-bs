fe8cae79908c7d7bab4d8c9513d362f86eb55a0f
Diya O'Brien <dbrien.diya@gmail.com>
2023-05-16 19:42:23 -0500

    color card components made

--- src/stories/components/Colors.stories.jsx
+++ src/stories/components/Colors.stories.jsx
@@ -1,34 +1,16 @@
-import ColorsExample from './Colors'; 
+// import ColorsExample from './Colors';  
+import ColorCards from './ColorCards';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+
 export default {
   title: 'Colors',
-  component: ColorsExample,
+  component: ColorCards,
   tags: ['autodocs'],
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
 };
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args 
 
-export const Success = {
-  args: {
-    primary: true,
-    wcag: 'AA 5.31' , 
-    number: 25, 
-    hex: "#F6FEF9"
-  },
-};
-
-export const Warning = {
-  args: {
-    ...Success.args,
-  },
-};
-
-export const Error = {
-  args: {
-    ...Success.args,
-  },
+export const Colors = {
+  
 };
