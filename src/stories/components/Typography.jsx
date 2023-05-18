aae5e998cd07e274a7fca041bd133cf61c537a4c
Diya O'Brien <dbrien.diya@gmail.com>
2023-05-17 22:44:02 -0500

    created list group items or cards for typography

--- src/stories/components/Typography.jsx
+++ src/stories/components/Typography.jsx
@@ -1,23 +1,44 @@
-import Card from 'react-bootstrap/Card';
+// import Badge from 'react-bootstrap/Badge';
 import ListGroup from 'react-bootstrap/ListGroup';
 
-export const ListGroupExample = ({}) => {
+export const DefaultExample = ({ heading, textName, textSize }) => {
   return (
-    <Card style={{ width: '18rem' }}>
-      <ListGroup variant="flush">
-      <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-      </ListGroup>
-    </Card>
+    <ListGroup style={{ width: '11rem', variant: "flush"}}>
+      <ListGroup.Item
+        as="li"
+        className="d-flex justify-content-between align-items-start">
+        <div className="ms-2 me-auto">{heading}</div>
+      </ListGroup.Item>
+      <ListGroup.Item
+        as="li"
+        className="d-flex justify-content-between align-items-start">
+        <div className="ms-2 me-auto">
+          <div>{textName}</div>
+          <div>{textSize}</div>
+        </div>
+      </ListGroup.Item>
+    </ListGroup>
   );
-}
+}; 
+
+export default DefaultExample;
+
+// import Card from 'react-bootstrap/Card';
+// import ListGroup from 'react-bootstrap/ListGroup';
+
+// export const ListGroupExample = ({ heading, textName, textSize }) => {
+//   return (
+//     <Card style={{ width: '18rem' }}>
+//       <ListGroup variant="flush">
+//         <ListGroup.Item>{heading}</ListGroup.Item>
+//         <Card.Text>{textName}</Card.Text>
+//         <Card.Text>{textSize}</Card.Text>
+//       </ListGroup>
+//     </Card>
+//   );
+// }
+
+// export default ListGroupExample;
 
 // export default ListGroupExample;
 
